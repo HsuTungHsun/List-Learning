f887d332d708a1cf58cec718da3bcdb90c02f621
Thomas Hsu <th30845@gmail.com>
2019-09-20 15:09:21 +0800

    add list

---
+++ list.py
@@ -0,0 +1,9 @@
+#list
+a = ['Toyota', 'Honda', 'Audi', True, 110, 11.5]
+#      a[0]      a[1]     a[2]   a[3]  a[4] a[5]
+print(a[0])
+a.append('Audi')  #add 'Audi' in a list
+print(a)
+print(len(a))      #length,how many block in list
+print('Audi' in a) #Check 'Audi in a list',True or False
+print('Benz' in a)
